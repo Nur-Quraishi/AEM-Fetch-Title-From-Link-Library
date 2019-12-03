23258604be0ba2f03fb433f68d98885f9da11ca8
M. A. Nur Quraishi <bsse0615@iit.du.ac.bd>
2019-12-03 16:05:01 +0600

    Update FetchLinkTitleServlet.java

--- core/src/main/java/com/myProject/core/servlets/FetchLinkTitleServlet.java
+++ core/src/main/java/com/myProject/core/servlets/FetchLinkTitleServlet.java
@@ -14,8 +14,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.Servlet;
-import java.io.IOException;
-import java.rmi.ServerException;
 
 @Component(service = Servlet.class,
         property = {
