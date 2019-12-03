8669e120b4170380e6e7219ad0096125ecc9b724
M. A. Nur Quraishi <bsse0615@iit.du.ac.bd>
2019-12-03 15:41:01 +0600

    Add files via upload

---
+++ link-to-title.js
@@ -0,0 +1,62 @@
+/**
+ * Extension to the standard checkbox component. It enables showing/hiding of other components based on the
+ * selection made in the checkbox.
+ *
+ * How to use:
+ *
+ * - add the class cq-dialog-checkbox-showhide to the checkbox element
+ * - add the data attribute cq-dialog-checkbox-showhide-target to the checkbox element, value should be the
+ *   selector, usually a specific class name, to find all possible target elements that can be shown/hidden.
+ * - add the target class to each target component that can be shown/hidden
+ * - add the class hidden to each target component to make them initially hidden
+ * - add the attribute showhidetargetvalue to each target component, the value should equal the value of the select
+ *   option that will unhide this element.  Leave this value as an empty string to toggle the target component on
+ *   when the checkbox is unchecked.
+ */
+(function(document, $) {
+    "use strict";
+
+    // when dialog gets injected
+    $(document).on("foundation-contentloaded", function(e) {
+        // if there is already an inital value make sure the according target element becomes visible
+        $(".cq-dialog-link-to-title").each( function() {
+            var value = $(this).find("[type='text']").val();
+            if(!value){
+                fetchTitle($(this));
+            }
+        });
+
+    });
+
+    $(document).on("change", ".cq-dialog-link-to-title", function(e) {
+        fetchTitle($(this));
+    });
+
+    function fetchTitle(el){
+        // get the selected value
+        var value = el.find("[type='text']").val();
+
+        // get the selector to find the target elements. its stored as data-.. attribute
+        var target = el.data("cqDialogLinkToTitleTarget");
+
+        if(value){
+            $.ajax({
+                url: '/bin/public/abbvie-pro/fetch-link-Title',
+                type: 'GET',
+                data: {
+                    'link' : value
+                },
+                success: function (title) {
+                    if(title){
+                        $(target).val(title);
+                    }
+                },
+                error: function(request, error){
+                    console.log("Request: " + JSON.stringify(request) + "\n" + "Error: " + JSON.stringify(error));
+                }
+            });
+        }
+
+    }
+
+})(document,Granite.$);
